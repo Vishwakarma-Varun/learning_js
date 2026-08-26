93a0aa47266d08070e834604e2acacfc45597bd3
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-08-26 07:23:28 +0000

    upload 04

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+/*
+    Equality check == and comparisons < > <= >= works differntly
+    comparison converts null to a number treating it as a 0 
+*/
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strictly equality check it compares the value of same data type
+
+console.log("2" === 2);
