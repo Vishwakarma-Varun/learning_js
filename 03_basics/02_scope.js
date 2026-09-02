7f134ea3849e39b5e367ff6c213ff5cfbb4f7f71
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-02 05:50:32 +0000

    more on scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -17,3 +17,56 @@ outside of this called global scope
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Varun"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "varun"
+    if (username === "varun") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+/*
+in scope its like parent cannot use the stuff of their child but child
+can use the stuff of parents. 
+*/
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+/*
+when function is created normally it can be acces in any order but when 
+it is assigned to a varible it can only be accessed after its insalization.
+*/
