693dca8fdbbf7e39a761b9de85eec20e6007da88
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-02 04:50:13 +0000

    more on fuction

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -24,3 +24,46 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Varun"))
+
+// function CalculateCartPrice(num){
+//     return num
+// }
+
+/*
+if we paas more than 1 value so we have to use rest operator its
+just add 3 dots before parameter i.e ...num now it will create 
+an array of the numbers we will pass to our function it is similar
+to spread it depends on its use case i.e where we use it. suppose if we
+write (num1,num2,...num) then first two values will be assign to num1 and
+num2 respectively and the rest wiil get stored in num in the form of array
+*/
+
+function CalculateCartPrice(val1,val2,...num){
+    return num
+}
+// console.log(CalculateCartPrice(200,300,400,500));
+
+const user = {
+    userName : "varun",
+    price : 999
+}
+
+function handleObject(anyObject){
+    return `Hello ${anyObject.userName} and the price is ${anyObject.price}`
+}
+
+// console.log(handleObject(user))
+
+// console.log(handleObject({
+//     userName: "Vishal",
+//     price : 250
+// }));
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
