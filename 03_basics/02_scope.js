113704e5fdb655568a84879feef22def439a53a3
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-02 05:11:26 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+}
+
+/*
+object and scope both are different but both are denoted by {} brackets 
+scopes are generally associated with function, conditional statements and
+loops. the inside of this data structures are called block scope and the 
+outside of this called global scope
+*/
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
