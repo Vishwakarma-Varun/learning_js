b3c261a792eff2ef623fd17a85b709b9857f7e9f
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-05 15:41:58 +0000

    learn about do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loop
+
+// let i = 1
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;  
+// }
+
+let fruits = ["Apple","Banana", "Orange"]
+
+let index = 0
+
+// while(index < fruits.length){
+//     console.log(fruits[index]);
+//     index++
+// }
+
+let score = 11
+
+do {
+    console.log(score);
+    score++
+    
+} while (score <10);
