868a38d92945fd64fb0488f8842ce983b80db59e
Varun <vishwakarmavarun13@gmail.com>
2026-09-08 19:40:14 +0530

    colour switcher

---
+++ 07_projects/01_colour_changer/script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+buttons.forEach((button) => {
+    button.addEventListener('click',(e) => {
+        if (e.target.id === 'blue'){
+            body.style.backgroundColor= e.target.id
+        }
+        if (e.target.id === 'green'){
+            body.style.backgroundColor= e.target.id
+        }
+        if (e.target.id === 'orange'){
+            body.style.backgroundColor= e.target.id
+        }
+        if (e.target.id === 'pink'){
+            body.style.backgroundColor= e.target.id
+        }
+    })
+})
